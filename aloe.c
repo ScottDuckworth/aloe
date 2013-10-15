2a29ce270c77e54861e93b71426c3e882e0b1567
Scott Duckworth <sduckwo@clemson.edu>
2013-10-15 16:18:36 -0400

    update copyright information

--- aloe.c
+++ aloe.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Scott Duckworth
+ * Copyright 2013 Clemson University
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
